c9a52fc4e1fbd53cb14c03c63b12d01b5fd89a25
ddj82 <dnwnrudckf77@gmail.com>
2025-04-14 23:11:02 +0900

    깃허브 페이지 배포설정

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/maple/', // 👉 레포지토리 이름으로
   plugins: [react()],
   server: {
     port: 5064, // ⭐ 원하는 포트 번호로 변경
