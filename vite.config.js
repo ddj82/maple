5e67f5834966bf68f6b0c9da18f3e164ed64804b
ddj82 <dnwnrudckf77@gmail.com>
2025-04-15 00:08:03 +0900

    🚀 자동 배포: GitHub Pages Actions 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig(({ mode }) => {
+  // eslint-disable-next-line no-undef
   const env = loadEnv(mode, process.cwd())
 
   return {
